b64b788b748654f30ff4ba1e461659ff6b8bb097
Sergei <sergejgaponov713@gmail.com>
2019-03-28 20:47:53 +0400

    start page

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img className="content-img" src="https://cdn.theculturetrip.com/wp-content/uploads/2015/11/New-Zealand-%C2%A9-vichie81-Shutterstock.jpg" alt=""/>
+      </div>
+      <div>
+        avatar + description
+      </div>
+      <div>
+        my posts
+        <div>new post</div>
+      </div>
+      <div>post1</div>
+      <div>post2</div>
+    </div>
+  )
+}
+
+export default Profile;
